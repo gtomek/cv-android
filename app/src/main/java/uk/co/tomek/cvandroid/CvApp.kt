40e03d095f394602e66aea43cfff00beae833294
Tomek Giszczak <tgiszczak@gmail.com>
2019-04-09 22:59:34 +0100

    Created empty Android project with initialised Application

---
+++ app/src/main/java/uk/co/tomek/cvandroid/CvApp.kt
@@ -0,0 +1,47 @@
+package uk.co.tomek.cvandroid
+
+import android.app.Application
+import android.os.StrictMode
+import com.squareup.leakcanary.AndroidExcludedRefs
+import com.squareup.leakcanary.LeakCanary
+import timber.log.Timber
+
+class CvApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+            setupLeakCanary()
+            enableStrictMode()
+        }
+    }
+
+    private fun enableStrictMode() {
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .penaltyFlashScreen()
+                .build())
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build())
+    }
+
+    private fun setupLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.refWatcher(this)
+            .excludedRefs(AndroidExcludedRefs.createAppDefaults().build())
+            .buildAndInstall()
+    }
+}
